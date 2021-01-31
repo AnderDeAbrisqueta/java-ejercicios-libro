f73de1123195dd6390646a7265c7d35d4e4410eb
Ander De Abrisqueta Rangel <ander.deabrisqueta.alu@iescampanillas.com>
2021-01-31 13:57:08 +0100

    Archivo que contiene las funciones 1-14 del tema 8

--- tema-8/misfunciones/Matematicas.java
+++ tema-8/misfunciones/Matematicas.java
@@ -79,7 +79,7 @@ public class Matematicas {
    * Esta función determina el siguiente número primo de un número introducido
    * por teclado
    *
-   * @param numero a partir del cual se buscará el número primo más próximo 
+   * @param numero a partir del cual se buscará el número primo más próximo
    * (mayor al número introducido).
    * @return un número primo.
    */
@@ -118,8 +118,8 @@ public class Matematicas {
    * entero positivo.
    *
    * @param numero entero positivo.
-   * @param n es la posición del dígito en el número, contado a partir del 0 y de
-   * izquierda a derecha.
+   * @param n es la posición del dígito en el número, contado a partir del 0 y
+   * de izquierda a derecha.
    * @return dígito del número en la posición elegida.
    */
   public static int digitoN(long numero, int n) {
@@ -149,25 +149,25 @@ public class Matematicas {
     }
     return numeroEsCapicua;
   }
-  
+
   /**
-   * Función que da la posición de la primera ocurrencia de un dígito
-   * dentro de un número entero. 
-   * 
-   * @param numero entero al que se le quiere determinar la posición de 
-   * un dígito.
+   * Función que da la posición de la primera ocurrencia de un dígito dentro de
+   * un número entero.
+   *
+   * @param numero entero al que se le quiere determinar la posición de un
+   * dígito.
    * @param digito: dígito al que se le quiere determinar la posición dentro del
    * número entero.
-   * @return la posición del dígito seleccionado, si no se encuentra, devuelve -1. 
-   * La posición del dígito en el número se cuenta a partir del 0 y de izquierda 
-   * a derecha.
+   * @return la posición del dígito seleccionado, si no se encuentra, devuelve
+   * -1. La posición del dígito en el número se cuenta a partir del 0 y de
+   * izquierda a derecha.
    */
   public static int posicionDeDigito(long numero, int digito) {
     digitos(numero);
     int posicionDigito = -1;
 
     for (int i = 0; i < digitos(numero); i++) {
-      int digitoEnNumero = (int) (numero / Math.pow(10, (digitos(numero) - (i + 1))));
+      int digitoEnNumero = (int) (numero / potencia(10, (digitos(numero) - (i + 1))));
 
       if (digitoEnNumero % 10 == digito) {
         posicionDigito = i;
@@ -176,49 +176,99 @@ public class Matematicas {
     }
     return posicionDigito;
   }
-  
+
   /**
    * Función que le quita a un número n dígitos por detrás (por la derecha).
-   * 
+   *
    * @param numero entero al que se le quieren quitar dígitos por la derecha.
-   * @param nDigitos número de dígitos que se quieren quitar.
+   * @param n número de dígitos que se quieren quitar.
    * @return el número cortado por la derecha.
    */
-  public static long quitarPorDetras(long numero, int nDigitos) {
-    return (long) (numero / Math.pow(10, nDigitos));
+  public static long quitarPorDetras(long numero, int n) {
+    return (long) (numero / potencia(10, n));
   }
-  
+
   /**
    * Función que le quita a un número n dígitos por detrás (por la derecha).
-   * 
+   *
    * @param numero entero al que se le quieren quitar dígitos por la derecha.
-   * @param nDigitos número de dígitos que se quieren quitar.
+   * @param n número de dígitos que se quieren quitar.
    * @return el número cortado por la derecha.
    */
-  public static long quitarPorDetras(int numero, int nDigitos) {
-    return quitarPorDetras((long) numero, (int) nDigitos);
+  public static long quitarPorDetras(int numero, int n) {
+    return quitarPorDetras((long) numero, (int) n);
   }
-  
+
   /**
    * Función que le quita a un número n dígitos por delante (por la izquierda).
-   * 
+   *
    * @param numero entero al que se le quieren quitar dígitos por la izquierda.
-   * @param nDigitos número de dígitos que se quieren quitar.
+   * @param n número de dígitos que se quieren quitar.
    * @return el número cortado por la izquierda
    */
-  public static long quitarPorDelante(long numero, int nDigitos) {
+  public static long quitarPorDelante(long numero, int n) {
     digitos(numero);
-    return numero % (long) Math.pow(10, digitos(numero) - nDigitos);
+    return numero % (long) potencia(10, digitos(numero) - n);
+  }
+
+  /**
+   * Función que le quita a un número n dígitos por delante (por la izquierda).
+   *
+   * @param numero entero al que se le quieren quitar dígitos por la izquierda.
+   * @param n número de dígitos que se quieren quitar.
+   * @return el número cortado por la izquierda
+   */
+  public static long quitarPorDelante(int numero, int n) {
+    return quitarPorDelante((long) numero, (int) n);
+  }
+
+  /**
+   * Función que añade dígitos a un número por detrás.
+   *
+   * @param numero al que se le quieren añadir dígitos por detrás.
+   * @param d dígitos a pegar por detrás.
+   * @return el número con los dígitos nuevos, pegados por detrás.
+   */
+  public static long pegaPorDetras(long numero, long d) {
+    return (numero * (long) potencia(10, digitos(d)) + d);
+  }
+
+  /**
+   * Función que añade dígitos a un número por delante.
+   *
+   * @param numero al que se le quieren añadir dígitos por delante.
+   * @param d dígitos a pegar por delante.
+   * @return el número con los dígitos nuevos, pegados por delante.
+   */
+  public static long pegaPorDelante(long numero, long d) {
+    return (d * (long) potencia(10, digitos(numero)) + numero);
   }
   
   /**
-   * Función que le quita a un número n dígitos por delante (por la izquierda).
+   * Función que toma como parámetros las posiciones inicial y final dentro de 
+   * un número y devuelve el trozo correspondiente.
    * 
-   * @param numero entero al que se le quieren quitar dígitos por la izquierda.
-   * @param nDigitos número de dígitos que se quieren quitar.
-   * @return el número cortado por la izquierda
+   * @param numero al del que se quiere un trozo
+   * @param x índice izquierdo del trozo. Se empieza contando por el 0 y de 
+   * izquierda a derecha.
+   * @param y índice derecho del trozo. Se empieza contando por el 0 y de 
+   * izquierda a derecha.
+   * @return el trozo de numero.
    */
-  public static long quitarPorDelante(int numero, int nDigitos) {
-    return quitarPorDelante((long) numero, (int) nDigitos);
+  public static long trozoDeNumero(long numero, int x, int y) {
+    long numeroCortadoDelante = quitarPorDelante(numero, x);
+    return (long) (numeroCortadoDelante / potencia(10, digitos(numero) - (y + 1)));
   }
+  
+  /**
+   * Función que pega dos números para formar uno.
+   * 
+   * @param x número que va por la izquierda.
+   * @param y número que va por la derecha.
+   * @return un número nuevo, resultado de juntar los números xy.
+   */
+  public static long juntaNumeros(long x, long y) {
+    return pegaPorDetras(x, y);
+  }
+  
 }
