780a000d7a7c2eebf26aca7adc44f3067ee6cbde
Ander De Abrisqueta Rangel <ander.deabrisqueta.alu@iescampanillas.com>
2021-03-10 08:41:00 +0100

    Realización del ejercicio 01 del tema 9 POO array

---
+++ tema-9/POOArray/ejercciocio01gato/Gato.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercciocio01gato;
+
+/**
+ *
+ * @author Ander De Abrisqueta Rangel
+ */
+public class Gato {
+
+  private String nombre;
+  private String raza;
+  private String color;
+
+  public Gato() {
+  }
+
+  public Gato(String nombre, String raza, String color) {
+    this.nombre = nombre;
+    this.raza = raza;
+    this.color = color;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public String getRaza() {
+    return raza;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public void setRaza(String raza) {
+    this.raza = raza;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  @Override
+  public String toString() {
+    return "Nombre: " + nombre + ", raza: " + raza + ", color: " + color;
+  }
+
+}
---
+++ tema-9/POOArray/ejercciocio01gato/mainGato.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercciocio01gato;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ander De Abrisqueta Rangel
+ */
+public class mainGato {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String args[]) {
+    Scanner s = new Scanner(System.in);
+    Gato[] gato = new Gato[4];
+    
+    System.out.println("Por favor, introduce los datos de los Gatos: ");
+    
+    for (int i = 0; i < 4; i++) {
+      System.out.print("Nombre: ");
+      String nombre = s.nextLine();
+      System.out.print("Raza: ");
+      String raza = s.nextLine();
+      System.out.print("Color: ");
+      String color = s.nextLine();
+      gato[i] = new Gato(nombre, raza, color);
+      System.out.println("");
+    }
+    
+    System.out.println("");
+    
+    for (Gato gato1 : gato) {
+      System.out.println(gato1);
+    }
+      
+  }
+}
